1323736248f250f927f8d49224ef0ffa933477e6
unknown <aj.halling7@gmail.com>
2019-11-07 14:14:38 -1000

    Encode/Decode/Brute with Comments

--- Caesar_Cypher/CaesarBrute.py
+++ Caesar_Cypher/CaesarBrute.py
@@ -1,21 +1,26 @@
 import sys
+#there's other ways to get the entire alphabet into a list, but just to be efficient and have less lines of code, it's just inputted like so.
 arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+#accept standard input, get each line into a list item, line 1 being the message and line 2 the key. 
 print("Please paste in your input and <CTRL-Z> when finished.")
 A = sys.stdin.read().splitlines()
 Input = []
 for item in A: 
     Input.append(str(item))
 
+#create the variable for Message
+#really, I could set it up so that there isn't a for loop and I just convert the stdin to a string but I'm tired and I want to go home
 Message = str(Input[0])
 
 print(Message)
 
+#loop through every possible key using a range function, from 0 to 25. For each key, do the same as what was done in the decoder file. 
 for i in range (0,26):
-    Encoded = ""
+    Decoded = ""
     for c in Message:
         if c.isalpha(): 
-            Encoded += arr[(arr.index(c)-i)%26]
-        else: Encoded += c
-    print("Key of "+str(i)+" decodes to: "+Encoded)
+            Decoded += arr[(arr.index(c)-i)%26]
+        else: Decoded += c
+    print("Key of "+str(i)+" decodes to: "+Decoded)
 
--- Caesar_Cypher/CaesarDecoder.py
+++ Caesar_Cypher/CaesarDecoder.py
@@ -1,22 +1,30 @@
 import sys
+#there's other ways to get the entire alphabet into a list, but just to be efficient and have less lines of code, it's just inputted like so.
 arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+#accept standard input, get each line into a list item, line 1 being the message and line 2 the key. 
 print("Please paste in your input and <CTRL-Z> when finished.")
 A = sys.stdin.read().splitlines()
 Input = []
 for item in A: 
     Input.append(str(item))
 
+#create the variables for Message and Key from the list I created above
 Message = str(Input[0])
 key = int(Input[1])
 
+#printing out the Message and Key with formatting
 print("Input Message: "+Message)
 print("Input Key: "+str(key))
 
+#creating a blank string to add the characters to
 Decoded = ""
+#for loop goes through each character in the "Message" string. If the character is an alpha character it uses the alphabet list above, the key, and the modulo operator to locate the new character and append it to the blank string. 
+#If it's not an alpha character (such as a space) it's skipped and appended to the string.
 for c in Message:
     if c.isalpha(): 
         Decoded += arr[(arr.index(c)-key)%26]
     else: Decoded += c
 
+#print out the decoded message with formatting. 
 print("The decoded message with a key of "+str(key)+" is: "+Decoded)
